fe140a35f7386a4367c553b5d4b512420bd83743
Scorpio493.8 <Scorpio493.8@gmail.com>
2024-06-12 19:19:52 +0800

    update model basic posture for mob pestilence_zombie

--- src/main/java/com/scorpio4938/fantasia/entity/client/PestilenceZombieModel.java
+++ src/main/java/com/scorpio4938/fantasia/entity/client/PestilenceZombieModel.java
@@ -9,27 +9,12 @@ import net.minecraft.client.util.math.MatrixStack;
 // Made with Blockbench 4.10.3
 // Exported for Minecraft version 1.17+ for Yarn
 // Paste this class into your mod and generate all required imports
+// Note. The model parts (folders in Blockbench) from Blockbench could create issues which minecraft client cannot read this model
 public class PestilenceZombieModel<T extends PestilenceZombieEntity> extends SinglePartEntityModel<T> {
     private final ModelPart pestilence_zombie;
-//    private final ModelPart body;
-//    private final ModelPart torso;
-//    private final ModelPart head;
-//    private final ModelPart bone;
-//    private final ModelPart right_arm;
-//    private final ModelPart left_arm;
-//    private final ModelPart right_leg;
-//    private final ModelPart left_leg;
 
     public PestilenceZombieModel(ModelPart root) {
         this.pestilence_zombie = root.getChild("pestilence_zombie");
-//        this.body = root.getChild("body");
-//        this.torso = root.getChild("torso");
-//        this.head = root.getChild("head");
-//        this.bone = root.getChild("bone");
-//        this.right_arm = root.getChild("right_arm");
-//        this.left_arm = root.getChild("left_arm");
-//        this.right_leg = root.getChild("right_leg");
-//        this.left_leg = root.getChild("left_leg");
     }
 
     public static TexturedModelData getTexturedModelData() {
@@ -48,17 +33,17 @@ public class PestilenceZombieModel<T extends PestilenceZombieEntity> extends Sin
 
         ModelPartData bone = torso.addChild("bone", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -24.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.0F))
                 .uv(34, 29).cuboid(-4.0F, -22.0F, 2.0F, 8.0F, 8.0F, 2.0F, new Dilation(0.0F))
-                .uv(44, 6).cuboid(0.0F, -14.0F, 2.0F, 4.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+                .uv(44, 6).cuboid(-1.0F, -14.0F, 2.0F, 4.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
         ModelPartData right_arm = body.addChild("right_arm", ModelPartBuilder.create().uv(34, 39).cuboid(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(-5.0F, -22.0F, 0.0F, -1.3963F, -0.0873F, 0.0F));
 
         ModelPartData left_arm = body.addChild("left_arm", ModelPartBuilder.create().uv(22, 12).cuboid(-1.0F, -3.0F, -2.0F, 4.0F, 13.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(5.0F, -22.0F, 0.0F, -1.309F, 0.0873F, 0.0F));
 
         ModelPartData right_leg = body.addChild("right_leg", ModelPartBuilder.create().uv(38, 10).cuboid(-2.0F, 0.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F))
-                .uv(14, 45).cuboid(1.0F, 0.0F, -2.0F, 1.0F, 9.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(-1.9F, -12.0F, 0.0F, -0.4363F, 0.0F, 0.0873F));
+                .uv(14, 45).cuboid(1.0F, 0.0F, -2.0F, 1.0F, 9.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(-1.9F, -12.0F, 0.0F, 0.0F, 0.0F, 0.0873F));
 
         ModelPartData left_leg = body.addChild("left_leg", ModelPartBuilder.create().uv(18, 29).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
-                .uv(37, 0).cuboid(-2.0F, 11.0F, -3.0F, 5.0F, 1.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(1.9F, -12.0F, 0.0F, 0.3491F, 0.0F, 0.0F));
+                .uv(37, 0).cuboid(-2.0F, 11.0F, -3.0F, 5.0F, 1.0F, 5.0F, new Dilation(0.0F)), ModelTransform.pivot(1.9F, -12.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
 
@@ -75,5 +60,4 @@ public class PestilenceZombieModel<T extends PestilenceZombieEntity> extends Sin
     public ModelPart getPart() {
         return this.pestilence_zombie;
     }
-
 }
